2489506e6c939e451342c95fecca189244738cbf
FelipeSouza14 <felipe.dev.2003@gmail.com>
2026-09-07 17:42:45 -0300

    chore: instalar dependencias e iniciar o projeto

---
+++ portfolio/app/(public)/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <div className="">
+            
+            <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-slate-200">
+                aaa
+            </div>
+
+        </div>
+    );
+}
