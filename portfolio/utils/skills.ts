fbb92ee500bf50cfe0b8c0334621834f11ec66d2
FelipeSouza14 <felipe.dev.2003@gmail.com>
2026-09-08 18:07:04 -0300

    feat: add uma estruturação em json dos dados correspondentes de cada habilidade para uma exibição mais eficiente e dinâmica

---
+++ portfolio/utils/skills.ts
@@ -0,0 +1,155 @@
+import { Brain, Database, Monitor, Server, Wrench } from "lucide-react";
+
+export const skills = [
+        {
+            id: "frontend",
+            icon: Monitor,
+            title: "Front-End",
+            stacks: [
+                {
+                    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg",
+                    name: "TypeScript",
+                    label: "Icone do TypeScript"
+                },
+                {
+                    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+                    name: "React",
+                    label: "Icone do React"
+                },
+                {
+                    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/reactnative/reactnative-original.svg",
+                    name: "React Native",
+                    label: "Icone do React Native"
+                },
+                {
+                    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/angular/angular-original.svg",
+                    name: "Angular",
+                    label: "Icone do Angular"
+                },
+                {
+                    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg",
+                    name: "Next.js",
+                    label: "Icone do Nextjs"
+                },
+                {
+                    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+                    name: "Tailwind CSS",
+                    label: "Icone do Tailwind"
+                },
+                
+            ],
+        },
+        {
+            id: "backend",
+            icon: Server,
+            title: "Back-End",
+            stacks: [
+                {
+                    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg",
+                    name: "Node.js",
+                    label: "Icone do Node.js"
+                },
+                {
+                    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nestjs/nestjs-original.svg",
+                    name: "NestJS",
+                    label: "Icone do NestJS"
+                },
+                {
+                    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/django/django-plain.svg",
+                    name: "Django",
+                    label: "Icone do Django"
+                },
+                {
+                    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/prisma/prisma-original.svg",
+                    name: "Prisma",
+                    label: "Icone do Prisma"
+                },
+            ],
+        },
+        {
+            id: "dataBase",
+            icon: Database,
+            title: "Banco de Dados",
+            stacks: [
+                {
+                    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg",
+                    name: "PostgreSQL",
+                    label: "Icone do PostgreSQL"
+                },
+                {
+                    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg",
+                    name: "MongoDB",
+                    label: "Icone do MongoDB"
+                },
+                {
+                    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original.svg",
+                    name: "MySQL",
+                    label: "Icone do MySQL"
+                },
+                {
+                    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sqlite/sqlite-original.svg",
+                    name: "SQLite",
+                    label: "Icone do SQLite"
+                },
+            ],
+        },
+        {
+            id: "dataSciense",
+            icon: Brain,
+            title: "Ciência de Dados",
+            stacks: [
+                {
+                    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg",
+                    name: "Python",
+                    label: "Icone do Python"
+                },
+                {
+                    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/googlecolab/googlecolab-original.svg",
+                    name: "Google Colab",
+                    label: "Icone do Google Colab"
+                },
+                {
+                    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/pandas/pandas-original.svg",
+                    name: "Pandas",
+                    label: "Icone do Pandas"
+                },
+                {
+                    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/scikitlearn/scikitlearn-original.svg",
+                    name: "Scikit-Learn",
+                    label: "Icone do Scikit-Learn"
+                },
+                {
+                    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/pytorch/pytorch-original.svg",
+                    name: "PyTorch",
+                    label: "Icone do PyTorch"
+                },
+            ],
+        },
+        {
+            id: "tools",
+            icon: Wrench,
+            title: "Ferramentas",
+            stacks: [
+                {
+                    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg",
+                    name: "Git",
+                    label: "Icone do Git"
+                },
+                {
+                    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg",
+                    name: "GitHub",
+                    label: "Icone do GitHub"
+                },
+                {
+                    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/trello/trello-original.svg",
+                    name: "Trello",
+                    label: "Icone do Trello"
+                },
+                {
+                    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/notion/notion-original.svg",
+                    name: "Notion",
+                    label: "Icone do Notion"
+                },
+            ],
+        },
+    ];
