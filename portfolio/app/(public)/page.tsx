60a18c1caad3e8b564235ad74aa0730501517700
FelipeSouza14 <felipe.dev.2003@gmail.com>
2026-09-09 21:11:51 -0300

    feat: criar o footer da página e importar os componentes

--- portfolio/app/(public)/page.tsx
+++ portfolio/app/(public)/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 // import { Header } from "./_components/header";
-import { Hero } from "./_components/hero";
-import { About } from "./_components/about";
-import SpaceBackground from "./_components/space-background";
+import { Hero } from "./_components/hero/hero";
+import { About } from "./_components/about/about";
+import SpaceBackground from "./_components/space-background-dois";
 import { Skills } from "./_components/skills/skills";
 import { Experience } from "./_components/experience/experience";
+import { Projects } from "./_components/projects/projects";
+import { Footer } from "./_components/footer/footer";
+import { Header } from "./_components/header/header";
+import { Education } from "./_components/education/education";
 
 export default function Home() {
     return (
@@ -14,10 +18,14 @@ export default function Home() {
             </div>
 
             <div className="relative z-10">
+                <Header />
                 <Hero />
                 <About />
                 <Skills />
                 <Experience />
+                <Projects />
+                <Education />
+                <Footer />
             </div>
         </main>
     );
